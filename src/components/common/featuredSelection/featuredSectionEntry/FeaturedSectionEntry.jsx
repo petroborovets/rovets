4652565c43f4bb332d7783b4e6bff6cf72f0b607
Petro <petroborovets@gmail.com>
2024-05-08 15:33:57 +0300

    3. Load component data from json files (#3)

--- src/components/common/featuredSelection/featuredSectionEntry/FeaturedSectionEntry.jsx
+++ src/components/common/featuredSelection/featuredSectionEntry/FeaturedSectionEntry.jsx
@@ -1,17 +1,17 @@
 import s from './FeaturedSectionEntry.module.css';
 
+import categoriesJson from './../../../../constants/categories.json'
+
 /* eslint-disable react/prop-types */
 function FeaturedSectionEntry(props) {
-    let image = props.image;
-    let category = props.category;
-    let title = props.title;
-    let date = props.date;
+    const {image, category, title, date} = props
+    const categoryName = categoriesJson?.categories?.filter(c => c.id === category)[0]?.name || 'Unknown Category';
 
     return <div className={s.section_entry}>
         <a href="">
             <img src={image} alt="photo"/>
         </a>
-        <a href={""} className={s.section_entry_cat}>{category}</a>
+        <a href={""} className={s.section_entry_cat}>{categoryName}</a>
         <h2 className={s.section_entry_title}>{title}</h2>
         <span className={s.section_entry_date}>{date}</span>
     </div>
