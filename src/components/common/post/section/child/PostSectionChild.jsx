eee04a47ca72429a88a05c8f908b2bf07cc09688
pborovets <petroborovets@gmail.com>
2024-05-11 16:43:56 +0300

    7. Post page impl, code review fixes

--- src/components/common/post/section/child/PostSectionChild.jsx
+++ src/components/common/post/section/child/PostSectionChild.jsx
@@ -2,27 +2,30 @@ import s from "./PostSectionChild.module.css";
 import {ORIGIN_URL} from "../../../../../constants/pathConstants.js";
 
 /* eslint-disable react/prop-types */
+const Image = ({sectionChild}) => <img className={s.post_section_img_child} src={ORIGIN_URL + sectionChild.value} alt=""/>
+const Paragraph = ({sectionChild}) => <p className={s.post_section_paragraph_child} dangerouslySetInnerHTML={{__html: sectionChild.value}} />
+const List = ({sectionChild}) => (
+    <ul>
+        {
+            sectionChild.value?.map(li =>
+                <li className={s.post_section_list_child} key={li}>{li}</li>
+            )
+        }
+    </ul>
+)
+
 function PostSectionChild(props) {
-    const sectionChild = props.sectionChild;
+    const {sectionChild} = props;
 
-    let element;
-    if (sectionChild.type === 'image') {
-        element = <img className={s.post_section_img_child} src={ORIGIN_URL + sectionChild.value} alt=""/>
-    } else if (sectionChild.type === 'paragraph') {
-        element = <p className={s.post_section_paragraph_child}>{sectionChild.value}</p>
-    } else if (sectionChild.type === 'list') {
-        element = <ul>
-            {
-                sectionChild.value?.map(li =>
-                    <li className={s.post_section_list_child} key={li} >{li}</li>
-                )
-            }
-        </ul>
+    const renderMap = {
+        image: Image,
+        paragraph: Paragraph,
+        list: List
     }
-
+    const Component = renderMap[sectionChild.type] || null;
     return (
         <div className={s.post_section_child}>
-            {element}
+            {Component && <Component sectionChild={sectionChild} />}
         </div>
     )
 }
