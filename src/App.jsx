491a1593380e6cd3c2a480a9f8c8928b056e54ae
pborovets <petroborovets@gmail.com>
2024-05-12 00:41:46 +0300

    11. fixes after testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css"
 import HomePage from "./pages/home/HomePage.jsx"
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createHashRouter, RouterProvider} from "react-router-dom";
 import AboutPage from "./pages/about/AboutPage.jsx";
 import ContactPage from "./pages/contact/ContactPage.jsx";
 import PostPage from "./pages/post/PostPage.jsx";
 import MainPageLayout from "./layouts/MainPageLayout.jsx";
 import CategoryPage from "./pages/category/CategoryPage.jsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: '/',
         element: <MainPageLayout/>,
