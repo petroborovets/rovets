af6f3c18e47925b3efa1657497b1f63975182b50
pborovets <petroborovets@gmail.com>
2024-05-25 16:42:11 +0300

    20. minor enhancements and fixes

--- src/layouts/MainPageLayout.jsx
+++ src/layouts/MainPageLayout.jsx
@@ -1,4 +1,6 @@
 import {Outlet} from "react-router-dom";
+import s from "./MainPageLayour.module.css"
+
 import Header from "../components/header/Header.jsx";
 import Footer from "../components/footer/Footer.jsx";
 import PreLoader from "../components/common/preLoader/PreLoader.jsx";
@@ -6,7 +8,7 @@ import PreLoader from "../components/common/preLoader/PreLoader.jsx";
 
 const MainPageLayout = () => {
     return (
-        <div>
+        <div className={s.main_page_layout}>
             <PreLoader />
             <Header/>
             <Outlet/>
