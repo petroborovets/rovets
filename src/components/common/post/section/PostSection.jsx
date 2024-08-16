bdb757db7676bd79d2f8ea8ea9d73c86e7450161
pborovets <petroborovets@gmail.com>
2024-08-16 11:13:00 +0300

    23. Section heading link improvements

--- src/components/common/post/section/PostSection.jsx
+++ src/components/common/post/section/PostSection.jsx
@@ -6,37 +6,50 @@ import {useState} from "react";
 /* eslint-disable react/prop-types */
 function PostSection({section, postId}) {
     const [isLinkClicked, setLinkClicked] = useState(false);
+    const [isHeadingHoovered, setHeadingHoovered] = useState(false);
 
     const sectionIdParam = "?section-id=" + section.id;
     const postSectionURLPath = "/rovets/post/" + postId + sectionIdParam;
 
-    function onLinkClick() {
-        const href = window.location.href
-        if (href.includes(postSectionURLPath)) {
-            navigator.clipboard.writeText(href)
-        } else {
-            navigator.clipboard.writeText(href.split('?')[0] + sectionIdParam)
-        }
+    function copyLinkToClipBoard() {
+        const href = window.location.href;
+        const urlToCopy = href.includes(postSectionURLPath)
+            ? href
+            : href.split('?')[0] + sectionIdParam;
 
+        navigator.clipboard.writeText(urlToCopy);
+    }
+
+    function onLinkClick() {
+        copyLinkToClipBoard();
         setLinkClicked(true)
         setTimeout(() => setLinkClicked(false), 2000);
     }
 
+    function onHeadingMouseEnter() {
+        setHeadingHoovered(true)
+    }
+
+    function onHeadingMouseLeave() {
+        setHeadingHoovered(false)
+    }
+
     return (
         <>
-            <ul className={s.post_section_heading}>
+            <ul className={s.post_section_heading} onMouseEnter={onHeadingMouseEnter}
+                onMouseLeave={onHeadingMouseLeave}>
                 <h1 id={section.id} className={s.post_section_heading_text}>{section.heading}</h1>
                 {
                     section.heading !== "" ?
                         <Link to={postSectionURLPath}
-                              className={s.post_section_link} onClick={onLinkClick}>
+                              className={`${s.post_section_link} ${isHeadingHoovered ? s.post_section_link_visible : ''}`}
+                              onClick={onLinkClick}>
                             {isLinkClicked ? '📋' : '🔗'}
                         </Link>
                         : ""
-
                 }
-
             </ul>
+
             {
                 section.children?.map(sectionChild =>
                     <PostSectionChild key={sectionChild.id} sectionChild={sectionChild}/>
